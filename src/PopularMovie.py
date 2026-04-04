2233be7db07c38357340fa190f1d91cc2274fc95
MsteR7 <ester.ngindu-mubiala@epitech.eu>
2026-04-04 23:20:47 +0200

    🚧 Add a Facebook class to improve the code's structure

--- src/PopularMovie.py
+++ src/PopularMovie.py
@@ -1,9 +1,10 @@
 
 class User:
     def __init__(self, name, friends, movies):
+        self.__id
         self.__name = name
-        self.__friends = friends
-        self.__moviesList = movies
+        self.__friends = []
+        self.__moviesList = []
 
     def get_name(self):
         return self.__name
@@ -14,10 +15,39 @@ class User:
     def get_moviesList(self):
         return self.__moviesList
 
+    def set_userId(self, id):
+        self.__id = id
+
+    def displayFriends(self):
+        for friend in self.__friends:
+            print(friend)
+
 
 class Movie:
     def __init__(self, title):
+        self.__id
         self.title = title
+
+    def set_movieId(self, id):
+        self.__id = id
+
+
+class Facebook:
+    def __init__(self):
+        self.__users = []
+        self.__movies = []
+
+    def addUser(self, user: User):
+        self.users.append(user)
+
+    def addMovies(self, movie: Movie):
+        self.movies.append(movie)
+
+    def getMovies(self):
+        return self.movies
+
+    def getUsers(self):
+        return self.users
 
 
 def MostPopularMovieOnNetwork():
