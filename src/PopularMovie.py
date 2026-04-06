72819719e9b4830e248ab82f34f586d2ce22b02c
MsteR7 <ester.ngindu-mubiala@epitech.eu>
2026-04-06 22:48:39 +0200

    ⚡️ Improve code's logic on classes and setup tests

--- src/PopularMovie.py
+++ src/PopularMovie.py
@@ -1,54 +1,54 @@
 
 class User:
     def __init__(self, name, friends, movies):
-        self.__id
         self.__name = name
-        self.__friends = []
-        self.__moviesList = []
+        self.__friendsList = friends
+        self.__moviesList = movies
 
     def get_name(self):
         return self.__name
 
     def get_friends(self):
-        return self.__friends
+        return self.__friendsList
 
     def get_moviesList(self):
         return self.__moviesList
 
-    def set_userId(self, id):
-        self.__id = id
-
     def displayFriends(self):
-        for friend in self.__friends:
+        for friend in self.__friendsList:
             print(friend)
 
 
 class Movie:
     def __init__(self, title):
-        self.__id
         self.title = title
 
-    def set_movieId(self, id):
-        self.__id = id
 
 
 class Facebook:
     def __init__(self):
-        self.__users = []
+        self.__users = {}
         self.__movies = []
 
     def addUser(self, user: User):
-        self.users.append(user)
+        self.__users[user.get_name()] = [user.get_friends(), user.get_moviesList()]
 
     def addMovies(self, movie: Movie):
-        self.movies.append(movie)
+        self.__movies.append(movie.title)
 
     def getMovies(self):
-        return self.movies
+        return self.__movies
+
+    def getMovieById(self, id):
+        id -= 1
+        for index, movie in enumerate(self.__movies):
+            if index == id:
+                return movie
+
 
     def getUsers(self):
-        return self.users
+        return self.__users
 
 
-def MostPopularMovieOnNetwork():
-    return None
+def MostPopularMovieOnNetwork(user):
+    return "None"
